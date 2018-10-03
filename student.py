6422bfdcaf4d4e8f7469c70147a6ddc378d5af39
digiandomenicos <digiandomenicos@gilmour.org>
2018-10-03 14:25:44 -0400

    safe to dance fix

--- student.py
+++ student.py
@@ -59,7 +59,7 @@ class Piggy(pigo.Pigo):
     def dance(self):
         """executes a series of methods that add up to a compound dance"""
         if not self.safe_to_dance():
-            print("\n----NOT SAVE TO DANCE----\n")
+            print("\n----NOT SAFE TO DANCE----\n")
         print("\n---- LET'S DANCE ----\n")
         ##### WRITE YOUR FIRST PROJECT HERE
         for x in range (1):
