90dd51a58f20d6b0e0c4db5252b136f626953dda
digiandomenicos <digiandomenicos@gilmour.org>
2018-10-05 14:15:40 -0400

    repeat dance 3 times

--- student.py
+++ student.py
@@ -63,7 +63,7 @@ class Piggy(pigo.Pigo):
             return
         print("\n---- LET'S DANCE ----\n")
         ##### WRITE YOUR FIRST PROJECT HERE
-        for x in range (2):
+        for x in range (3):
             self.shuffle_forward()
             self.bob_head()
             self.spin()
