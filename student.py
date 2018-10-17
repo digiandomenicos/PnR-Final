f3723787f6d762f7165de97a831e575e4720538e
digiandomenicos <digiandomenicos@gilmour.org>
2018-10-17 14:37:06 -0400

    self.say_no make fix

--- student.py
+++ student.py
@@ -46,8 +46,7 @@ class Piggy(pigo.Pigo):
                 "c": ("Calibrate", self.calibrate),
                 "s": ("Check status", self.status),
                 "h": ("Open House", self.open_house),
-                "q": ("Quit", quit_now)
-                }
+                "q": ("Quit", quit_now)}
         # loop and print the menu...
         for key in sorted(menu.keys()):
             print(key + ":" + menu[key][0])
