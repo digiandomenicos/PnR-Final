f6cac919d0b28e63ec5553b9c3831cd8baf8b35a
digiandomenicos <digiandomenicos@gilmour.org>
2018-10-31 14:48:30 -0400

    if false

--- student.py
+++ student.py
@@ -290,10 +290,12 @@ class Piggy(pigo.Pigo):
             if self.is_clear():
                 self.cruise()
             #if the amount of space from the right is greater than the left go to the left
-            if False right_not_clear
+            if False right_not_clear:
+                pass
 
             #if the amount of space from the left is greater than the right go to the right
-            if False self.left_not_clear
+            if False self.left_not_clear:
+                pass
 
     def right_not_clear(self):
         left_total = 0
