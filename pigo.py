83bfe175804e66fae151bf667adcaff510309f21
digiandomenicos <digiandomenicos@gilmour.org>
2018-12-05 14:47:56 -0500

    making is clear smaller

--- pigo.py
+++ pigo.py
@@ -187,7 +187,7 @@ class Pigo(object):
     def is_clear(self):
         """does a 3-point scan around the midpoint, returns false if a test fails"""
         print("Running the is_clear method.")
-        for x in range((self.MIDPOINT - 60), (self.MIDPOINT + 60), 5):
+        for x in range((self.MIDPOINT - 45), (self.MIDPOINT + 45), 5):
             self.servo(x)
             scan1 = self.dist()
             # double check the distance
